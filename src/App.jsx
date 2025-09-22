4b176fbf6a18df28867c7e7ff64cdc710ed70c8b
Victor de Souza Vieira <victordesouzavieira@gmail.com>
2025-09-22 19:36:03 -0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Game from "./components/game";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>What color is this?</h1>
+      <Game />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Game.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import "./Game.css";
+
+function Game() {
+  const [RGBValues, setRGBValues] = useState([[]]);
+  const [correctOption, setCorrectOption] = useState([]);
+  const [nextRound, setNextRound] = useState(false);
+  const [hiddenButtons, setHiddenButtons] = useState([]);
+
+  useEffect(() => {
+    let values = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 3; j++) {
+        values[i][j] = Math.floor(Math.random() * 256);
+      }
+    }
+    setRGBValues(values);
+    const correctOption = values[Math.floor(Math.random() * 5)];
+    setCorrectOption(correctOption);
+    setHiddenButtons([]);
+  }, [nextRound]);
+
+  function chooseoption(rbgValue, index) {
+    if (rbgValue === correctOption) {
+      setNextRound(!nextRound);
+    } else {
+      setHiddenButtons((prev) => [...prev, index]);
+    }
+  }
+
+  return (
+    <div>
+      <h2>
+        {"Rgb (" +
+          correctOption[0] +
+          ", " +
+          correctOption[1] +
+          ", " +
+          correctOption[2] +
+          ")"}
+      </h2>
+      <div className="options-container">
+        {RGBValues.map((rbgValue, i) => (
+          <button
+            id={rbgValue}
+            onClick={() => chooseoption(rbgValue, i)}
+            key={i}
+            className="option-button"
+            style={{
+              backgroundColor:
+                "rgb(" +
+                rbgValue[0] +
+                "," +
+                rbgValue[1] +
+                "," +
+                rbgValue[2] +
+                ")",
+              visibility: hiddenButtons.includes(i) ? "hidden" : "visible",
+            }}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Game;
